40f8713b4edc29925b5e607d69cb87ff4b8cf3fc
zoverhevenz <122011111+zoverhevenz@users.noreply.github.com>
2023-10-03 18:24:26 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { seconds: 0 };
+  }
+
+  tick() {
+    this.setState((state) => ({
+      seconds: state.seconds + 1,
+    }));
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return <h1>seconds: {this.state.seconds}</h1>;
+  }
+}
+
+export default Timer;
